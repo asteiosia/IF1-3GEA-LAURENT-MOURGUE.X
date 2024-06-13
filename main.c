e73c9629953f235d2252fb040c56ea2e852ff8b5
asteiosia <155630912+asteiosia@users.noreply.github.com>
2024-06-13 12:29:31 +0200

    first_commit

--- main.c
+++ main.c
@@ -68,7 +68,7 @@
 #include <xc.h>
 
 void i2c_master_setup(void) ;
-
+void i2c_master_start(void) ;
 void i2c_master_restart(void);
 void i2c_master_send(unsigned char byte);
 unsigned char i2c_master_recv(void);
